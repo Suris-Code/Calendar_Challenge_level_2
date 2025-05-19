9c0d7bb4f2a7d7df48c3b8e16d55eeab03f95489
BeckerSuris <becker.portilla@suriscode.com>
2025-05-19 20:27:59 -0300

    Update HomePage.tsx

--- reactnet.client/src/pages/home/HomePage.tsx
+++ reactnet.client/src/pages/home/HomePage.tsx
@@ -3,7 +3,7 @@ const HomePage = () => {
   return (
     <div className="w-full px-6">
       <DashboardStats />
-      <span className="text-xs text-gray-500 flex justify-end pb-2 pe-2">Doble click para crear evento</span>
+      <span className="text-lg text-gray-500 flex justify-end pb-2 pe-2">Doble click para crear evento</span>
       <div className="flex-grow bg-white rounded-lg shadow-md overflow-hidden">
         <Calendar />
       </div>
